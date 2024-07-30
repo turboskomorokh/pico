56e5e72febe69470faea6a67860adba97902d558
Paul Goldstein <turboskomorokh@gmail.com>
2024-07-30 21:19:52 +0300

    damn

--- src/deps.c
+++ src/deps.c
@@ -1,14 +1,20 @@
+
 #include "db.h"
 #include "pkg.h"
 #include "xfunc.h"
 #include "env.h"
 #include "config.h"
+#include "config.h"
+#include "db.h"
+#include "env.h"
+#include "pkg.h"
+#include "xfunc.h"
 
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
-char *pkg_solve_dependencies(pkg_t *pkg) {
+int pkg_solve_dependencies(pkg_t *pkg) {
   char *ds, *d, *d_failed;
 
   ds = xstrdup(pkg->depends);
@@ -31,35 +37,78 @@ char *pkg_solve_dependencies(pkg_t *pkg) {
 
   if (d_failed[0] != '\0')
     pkg->state |= PICO_PKG_DEPS_FAIL;
-
-  return d_failed;
+  return !(pkg->state & PICO_PKG_DEPS_FAIL);
 }
 
-char *pkg_solve_conflicts(pkg_t *pkg) {
-  char *cs, *c, *c_failed;
+int pkg_solve_conflicts(pkg_t *pkg) {
+  char *cs, *csdb, *c, *c_failed_self, *c_failed;
 
   cs = xstrdup(pkg->conflicts);
+  csdb = xstrdup(pkg->conflicts);
 
-  c_failed = xmalloc(strlen(pkg->conflicts));
+  c_failed_self = xmalloc(strlen(pkg->conflicts) + 1);
+  c_failed_self[0] = '\0';
+
+  c_failed = xmalloc(strlen(pkg->conflicts) + 1);
   c_failed[0] = '\0';
 
   c = strtok(cs, " ,");
   while (c != NULL) {
     if (db_exists(c)) {
-      size_t d_size = strlen(c_failed) + strlen(c) + 2;
-      c_failed = xrealloc(c_failed, d_size);
-      strcat(c_failed, c);
-      strcat(c_failed, " ");
+      size_t cslf_size = strlen(c_failed_self) + strlen(c) + 2;
+      c_failed_self = xrealloc(c_failed_self, cslf_size);
+      strcat(c_failed_self, c);
+      strcat(c_failed_self, " ");
     }
     c = strtok(NULL, " ,");
   }
 
   free(cs);
-
-  if (c_failed[0] != '\0')
+  if (c_failed[0] != '\0') {
+    printf("%s conflict with these packages:\n%s\n", pkg->name, c_failed_self);
     pkg->state |= PICO_PKG_CONFS_FAIL;
+  }
 
-  return c_failed;
+  DIR *dir = xopendir(CONFIG_DB_DIR);
+  if (dir == NULL) {
+    free(c_failed);
+    return EXIT_FAILURE;
+  }
+
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+      char *full_path = string_compose(CONFIG_DB_DIR "/%s", entry->d_name);
+
+      struct stat st;
+      if (stat(full_path, &st) != 0 || !S_ISDIR(st.st_mode)) {
+        free(full_path);
+        continue;
+      }
+
+      pkg_t *pkg_db = db_get(entry->d_name);
+      if (pkg_db && pkg_db->conflicts && strstr(pkg_db->conflicts, pkg->name)) {
+        size_t new_size = strlen(c_failed) + strlen(pkg_db->name) + 2;
+        char *temp = xrealloc(c_failed, new_size);
+
+        c_failed = temp;
+        if (c_failed[0] != '\0') {
+          strcat(c_failed, " ");
+        }
+        strcat(c_failed, pkg_db->name);
+      }
+      free(full_path);
+    }
+  }
+
+  closedir(dir);
+
+  if (c_failed[0] != '\0') {
+    printf("These packages conflict with %s:\n%s\n", pkg->name, c_failed);
+    pkg->state |= PICO_PKG_CONFS_FAIL;
+  }
+
+  return !(pkg->state & PICO_PKG_CONFS_FAIL);
 }
 
 int pkg_solve_removal_dependencies(pkg_t *pkg) {
--- src/install.c
+++ src/install.c
@@ -24,65 +24,51 @@ int pkg_install(char *argument) {
   pkg->info = tar_read_file_buf(fp, "pico-package");
   if (pkg->info == NULL) {
     perror("Failed to read package info");
-    fclose(fp);
     free_pkg(pkg);
     return EXIT_FAILURE;
   }
-
-  xfseek(fp, 0, SEEK_SET);
+  fclose(fp);
 
   pkg_init_buf(pkg->info, pkg);
   printf("Selecting %s:%s (%s)\n", pkg->name, pkg->arch, pkg->version);
 
   if (pkg->state & PICO_PKG_INIT_FAIL) {
     error_msg("%s is not a package.\n", argument);
-    fclose(fp);
     free_pkg(pkg);
     return EXIT_FAILURE;
   }
 
   if (db_exists(pkg->name)) {
     error_msg("%s is already installed", pkg->name);
-    fclose(fp);
     free_pkg(pkg);
     return EXIT_SUCCESS;
   }
 
   if(pkg_check_arch(pkg)) {
     error_msg("%s have foreign architecture: %s", pkg->name, pkg->arch);
-    fclose(fp);
     free_pkg(pkg);
     return EXIT_SUCCESS;
   }
 
-  char *f_deps = pkg_solve_dependencies(pkg);
+  pkg_solve_dependencies(pkg);
   if (pkg->state & PICO_PKG_DEPS_FAIL) {
-    error_msg("%s depends on:\n%s\nInstall these first.", pkg->name, f_deps);
-    fclose(fp);
     free_pkg(pkg);
-    free(f_deps);
     return EXIT_SUCCESS;
   }
 
-  char *f_confs = pkg_solve_conflicts(pkg);
+  pkg_solve_conflicts(pkg);
   if (pkg->state &
       PICO_PKG_CONFS_FAIL) {
-    error_msg("%s conflicts with:\n%s", pkg->name, f_confs);
-    fclose(fp);
     free_pkg(pkg);
-    free(f_deps);
-    free(f_confs);
     return EXIT_SUCCESS;
   }
 
-  free(f_deps);
-  free(f_confs);
-
+  fp = xfopen(argument, "r");
   pkg->list = tar_extract_source(fp);
+  fclose(fp);
 
   db_add(pkg);
 
-  fclose(fp);
   free_pkg(pkg);
 
   return EXIT_SUCCESS;
